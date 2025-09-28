7395b9f466fc6d21887bda0fbfdaa80a9159a471
mrosadevs <giftedlean@gmail.com>
2025-09-28 18:28:26 -0400

    completed code for Arrays excercise

--- In-Class-Assignment-Arrays/main.cpp
+++ In-Class-Assignment-Arrays/main.cpp
@@ -9,4 +9,39 @@ int main() {
         std::cout << numbers[i] << " ";
     }
     std::cout << std::endl;
+
+    for (int i = 0; i < size -1; i++) {
+        for (int j = 0; j < size -1 - i; j++) {
+            if (numbers [j] > numbers [j + 1]) {
+                int temp = numbers [j];
+                numbers [j] = numbers [j + 1];
+                numbers [j + 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "Ascending order: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size -1 - i; j++) {
+            if (numbers [j] < numbers [j + 1]) {
+                int temp = numbers [j];
+                numbers [j] = numbers [j + 1];
+                numbers [j + 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "Descending order: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+
 }
