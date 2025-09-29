4b52899a48371dc22032290d9b8aabfd770c7981
mrosadevs <giftedlean@gmail.com>
2025-09-29 16:07:31 -0400

    Completed Vector and Array Implementation Excercise

--- In-Class-Assignment-Vector-And-Array-Implementation/main.cpp
+++ In-Class-Assignment-Vector-And-Array-Implementation/main.cpp
@@ -14,5 +14,31 @@ int main()
     numbers.push_back(3.14); //This adds the first double to the vector.
     numbers.push_back(2.71); //This adds the second double to the vectors.
 
-    
+    //This loop displays teh vector after adding the 2 doubles.
+    std::cout << "After adding 2 elements: ";
+    for (double n : numbers) {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    numbers.pop_back(); //This removes the last double from the vector.
+
+    //This loop displays the vector after removing the last double.
+    std::cout << "After deleting 1 element:";
+    for (double n : numbers) {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    numbers.pop_back(); //This removes the last double from the vector.
+
+    //This loop displays the vector after removing the last double.
+    std::cout << "After deleting 1 element:";
+    for (double n : numbers) {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+
 }
