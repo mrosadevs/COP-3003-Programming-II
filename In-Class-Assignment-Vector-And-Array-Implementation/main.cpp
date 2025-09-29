8ebf72930bee450aa5dfc5af58cc4aea2b12b666
Manuel <giftedlean@gmail.com>
2025-09-29 11:41:46 -0400

    Started Vector and Array Implementation

--- In-Class-Assignment-Vector-And-Array-Implementation/main.cpp
+++ In-Class-Assignment-Vector-And-Array-Implementation/main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
 #include <vector>
 
-//test test
+int main()
+{
+    std::vector<double> numbers; //This creates an empty vector of doubles.
+
+    std::cout << "Initial Vector: "; //This displays the initial state of the vector.
+    for (double n :numbers) {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    numbers.push_back(3.14); //This adds the first double to the vector.
+    numbers.push_back(2.71); //This adds the second double to the vectors.
+
+    
+}
