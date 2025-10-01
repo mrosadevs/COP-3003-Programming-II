5904d31710dc904c6e4ccd9052405b2f5e2ceeea
Manuel <giftedlean@gmail.com>
2025-10-01 11:11:40 -0400

    Started pointers work.

--- In-Class-Work-Pointers/main.cpp
+++ In-Class-Work-Pointers/main.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+
+int main() {
+    double num = 20;
+    double *p = &num;
+
+    std::cout << "Value of num: " << num << std::endl;
+    std::cout << "Address of num: " << &num << std::endl;
+    std::cout << "Pointer p holds: " << p << std::endl;
+    std::cout << "Value at address stored in p: " << *p << std::endl;
+
+    return 0;
+}
