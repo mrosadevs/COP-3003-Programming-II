6da0a9740637c4fee687708a4ea227391bbd466e
Manuel <giftedlean@gmail.com>
2025-10-15 10:50:01 -0400

    Complted in class work

--- In-Class-Assignment-Passing-Techniques/main.cpp
+++ In-Class-Assignment-Passing-Techniques/main.cpp
@@ -1,3 +1,35 @@
-//
-// Created by Manuel Rosales on 10/15/25.
-//
+#include <iostream>
+
+int doubleByValue(int num) {
+    return num * 2;
+}
+
+void doubleByReference(int &num) {
+    num *= 2;
+}
+
+void doubleByPointer(int *num) {
+    *num *= 2;
+}
+
+int main() {
+    int number;
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    int valueNumber = doubleByValue(number);
+
+    int refNumber = number;
+    doubleByReference(refNumber);
+
+    int ptrNumber = number;
+    doubleByPointer(&ptrNumber);
+
+    std::cout << "\nResults:\n";
+    std::cout << "Calling by Value: " << valueNumber << std::endl;
+    std::cout << "Calling by Reference: " << refNumber << std::endl;
+    std::cout << "Calling by Pointer: " << ptrNumber << std::endl;
+
+    return 0;
+}
